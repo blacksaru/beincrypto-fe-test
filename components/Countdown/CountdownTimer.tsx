980b82f6d6054d77bba6ec21c4ed0761746b3764
venture1981 <bestlucky1030@gmail.com>
2023-08-12 03:19:34 +0900

    cls

--- components/Countdown/CountdownTimer.tsx
+++ components/Countdown/CountdownTimer.tsx
@@ -1,53 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Paper, Text } from '@mantine/core';
+import { useContract } from '../../contexts/ContracContext';
+import { ethers } from 'ethers';
+import { RPC_URL } from '../../config';
 
 interface CountdownTimerProps {
   targetTime: number;
 }
 
+const formatTimeSegment = (segment: number): string => {
+  return segment < 10 ? `0${segment}` : `${segment}`;
+};
+
 const CountdownTimer: React.FC<CountdownTimerProps> = ({ targetTime }) => {
-  // const [remainingTime, setRemainingTime] = useState<number>(targetTime - Date.now());
+  const [remainingTime, setRemainingTime] = useState<number>(0);
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setRemainingTime(targetTime - Date.now());
-  //   }, 1000);
+  const { currentStageStartTimeStamp, stageBlocksDuration } = useContract();
+  const stageBlocksDurationDouble = stageBlocksDuration * 2;
+  const provider = new ethers.JsonRpcProvider(RPC_URL);
 
-  //   return () => clearInterval(interval);
-  // }, [targetTime]);
+  const getBlockInfo = async () => {
+    // get remaining time
+    const blockNum = await provider.getBlockNumber();
+    const blockInfo = await provider.getBlock(blockNum);
+    if (blockInfo) {
+      const remain = currentStageStartTimeStamp + stageBlocksDurationDouble - blockInfo.timestamp;
+      setRemainingTime(remain * 1000);
+    }
+  };
 
-  // const hours = Math.floor(remainingTime / 3600000);
-  // const minutes = Math.floor((remainingTime % 3600000) / 60000);
-  // const seconds = Math.floor((remainingTime % 60000) / 1000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getBlockInfo();
+    }, 2000);
 
-  // const formatTimeSegment = (segment: number): string => {
-  //   return segment < 10 ? `0${segment}` : `${segment}`;
-  // };
+    return () => clearInterval(interval);
+  }, [targetTime]);
+
+  const hours = Math.floor(remainingTime / 3600000);
+  const minutes = Math.floor((remainingTime % 3600000) / 60000);
+  const seconds = Math.floor((remainingTime % 60000) / 1000);
 
   return (
-    <Paper padding="xl" shadow="xs">
+    <Paper>
       <Text
         align="center"
         sx={{
-          color: '#fff',
-          fontSize: 80,
+          fontSize: 60,
           fontWeight: 700,
           fontFamily: 'Inter',
           lineHeight: 1,
         }}
       >
-        {/* {formatTimeSegment(hours)} */}
-        776
+        {hours >= 0 ? formatTimeSegment(hours) : '--'}
         <Text component="span" size="md">
           H
         </Text>
-        {/* {formatTimeSegment(minutes)} */}
-        15
+        {minutes >= 0 ? formatTimeSegment(minutes) : '--'}
         <Text component="span" size="md">
           M
         </Text>
-        {/* {formatTimeSegment(seconds)} */}
-        15
+        {seconds >= 0 ? formatTimeSegment(seconds) : '--'}
         <Text component="span" size="md">
           S
         </Text>
--- contexts/ContracContext.tsx
+++ contexts/ContracContext.tsx
@@ -1,12 +1,6 @@
-import React, { createContext, useContext, ReactNode, useState, useEffect } from 'react';
+import React, { createContext, useContext, useMemo, ReactNode, useState, useEffect } from 'react';
 import { ethers } from 'ethers';
-import {
-    TOKEN_ADDRESS,
-    TOKEN_ABI,
-    PRESALE_ADDRESS,
-    PRESALE_ABI,
-    RPC_URL
-} from '../config';
+import { TOKEN_ADDRESS, TOKEN_ABI, PRESALE_ADDRESS, PRESALE_ABI, RPC_URL } from '../config';
 
 // Replace with your actual contract address and ABI
 const tokenContractAddress = TOKEN_ADDRESS;
@@ -16,41 +10,157 @@ const presaleContractAddress = PRESALE_ADDRESS;
 const presaleContractAbi = PRESALE_ABI;
 
 interface ContractContextProps {
-    tokenContract: ethers.Contract | null;
-    presaleContract: ethers.Contract | null;
+  tokenContract: ethers.Contract | null;
+  presaleContract: ethers.Contract | null;
+  tokenDecimals: number;
+  stageBlocksDuration: number;
+  stageMaxTokens: number;
+  stageMaxWalletBuy: number;
+  unitPrice: number;
+  blockStart: number;
+  currentStage: number;
+  currentStageAvailableAmount: number;
+  currentStageBlockStart: number;
+  currentStageMaxAmount: number;
+  paused: string;
+  tokenName: string;
+  tokenSymbol: string;
+  totalSupply: number;
+  currentBlockNumber: number;
+  getTokenInfo: () => void;
+  currentStageStartTimeStamp: number;
 }
 
 const ContractContext = createContext<ContractContextProps>({
-    tokenContract: null,
-    presaleContract: null
+  tokenContract: null,
+  presaleContract: null,
+  tokenDecimals: 0,
+  stageBlocksDuration: 0,
+  stageMaxTokens: 0,
+  stageMaxWalletBuy: 0,
+  unitPrice: 0,
+  blockStart: 0,
+  currentStage: 0,
+  currentStageAvailableAmount: 0,
+  currentStageBlockStart: 0,
+  currentStageMaxAmount: 0,
+  paused: 'False',
+  tokenName: '',
+  tokenSymbol: '',
+  totalSupply: 0,
+  getTokenInfo: () => {},
+  currentBlockNumber: 3882142,
+  currentStageStartTimeStamp: 0,
 });
 
 export const useContract = () => {
-    const context = useContext(ContractContext);
-    if (!context) {
-        throw new Error("useContract must be used within a ContractProvider");
-    }
-    return context;
+  const context = useContext(ContractContext);
+  if (!context) {
+    throw new Error('useContract must be used within a ContractProvider');
+  }
+  return context;
 };
 
 export const ContractProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [tokenContract, setTokenContract] = useState<ethers.Contract | null>(null);
-    const [presaleContract, setPresaleContract] = useState<ethers.Contract | null>(null);
+  let provider: ethers.Provider;
+  if (typeof window !== 'undefined' && window.ethereum) {
+    console.log('ethereum');
+    provider = new ethers.BrowserProvider(window.ethereum);
+  } else {
+    provider = new ethers.JsonRpcProvider(RPC_URL);
+  }
+  const tokenContract = new ethers.Contract(tokenContractAddress, tokenContractAbi, provider);
+  const presaleContract = new ethers.Contract(presaleContractAddress, presaleContractAbi, provider);
+  const [currentBlockNumber, setCurrentBlockNumber] = useState(38882142);
+  const [currentStageStartTimeStamp, setCurrentStageStartTimeStamp] = useState(0);
 
-    useEffect(() => {
-        const provider = new ethers.JsonRpcProvider(RPC_URL);
-        const contractInstanceT = new ethers.Contract(tokenContractAddress, tokenContractAbi, provider);
-        const contractInstanceS = new ethers.Contract(presaleContractAddress, presaleContractAbi, provider);
-        setTokenContract(contractInstanceT);
-        setPresaleContract(contractInstanceS);
-    }, []);
+  const [tokenDecimals, setTokenDecimals] = useState(18);
+  const [tokenName, setTokenName] = useState('');
+  const [tokenSymbol, setTokenSymbol] = useState('');
+  const [totalSupply, setTotalSupply] = useState(0);
 
-    return (
-        <ContractContext.Provider value={{
-            tokenContract,
-            presaleContract
-        }}>
-            {children}
-        </ContractContext.Provider>
-    );
+  const [stageBlocksDuration, setStageBlocksDuration] = useState(0);
+  const [stageMaxTokens, setStageMaxTokens] = useState(0);
+  const [stageMaxWalletBuy, setStageMaxWalletBuy] = useState(0);
+  const [unitPrice, setUnitPrice] = useState(0);
+  const [blockStart, setBlockStart] = useState(0);
+  const [currentStage, setCurrentStage] = useState(0);
+  const [currentStageAvailableAmount, setCurrentStageAvailableAmount] = useState(0);
+  const [currentStageBlockStart, setCurrentStageBlockStart] = useState(0);
+  const [currentStageMaxAmount, setCurrentStageMaxAmount] = useState(0);
+  const [paused, setPaused] = useState('False');
+
+  const getTokenInfo = async () => {
+    const decimals = await tokenContract.decimals();
+    const name = await tokenContract.name();
+    const symbol = await tokenContract.symbol();
+    const supply = await tokenContract.totalSupply();
+
+    const blockDuration = await presaleContract.STAGE_BLOCKS_DURATION();
+    const maxTokens = await presaleContract.STAGE_MAX_TOKENS();
+    const maxWalletBy = await presaleContract.STAGE_MAX_WALLET_BUY();
+    const unitP = await presaleContract.UNIT_PRICE();
+    const start = await presaleContract.blockStart();
+    const cuStage = await presaleContract.currentStage();
+    const ableAmount = await presaleContract.currentStageAvailableAmount();
+    const cuBlockStart = await presaleContract.currentStageBlockStart();
+    const cuMaxAmount = await presaleContract.currentStageMaxAmount();
+    const paus = await presaleContract.paused();
+
+    setStageBlocksDuration(Number(blockDuration));
+    setStageMaxTokens(Number(maxTokens) / Math.pow(10, Number(decimals)));
+    setStageMaxWalletBuy(Number(maxWalletBy) / Math.pow(10, Number(decimals)));
+    setUnitPrice(Number(unitP));
+    setBlockStart(Number(start));
+    setCurrentStage(Number(cuStage));
+    setCurrentStageAvailableAmount(Number(ableAmount) / Math.pow(10, Number(decimals)));
+    setCurrentStageBlockStart(Number(cuBlockStart));
+    setCurrentStageMaxAmount(Number(cuMaxAmount) / Math.pow(10, Number(decimals)));
+    setPaused(paus);
+
+    setTokenDecimals(Number(decimals));
+    setTokenName(name);
+    setTokenSymbol(symbol);
+    setTotalSupply(Number(supply));
+
+    const blockNum = await provider.getBlockNumber();
+    const blockData = await provider.getBlock(Number(cuBlockStart));
+
+    if (blockData) {
+      setCurrentStageStartTimeStamp(blockData.timestamp);
+    }
+    setCurrentBlockNumber(blockNum);
+  };
+
+  useEffect(() => {
+    getTokenInfo();
+  }, [tokenContract, presaleContract]);
+
+  return (
+    <ContractContext.Provider
+      value={{
+        currentBlockNumber,
+        currentStageStartTimeStamp,
+        tokenContract,
+        presaleContract,
+        tokenDecimals,
+        tokenName,
+        tokenSymbol,
+        totalSupply,
+        getTokenInfo,
+        stageBlocksDuration,
+        stageMaxTokens,
+        stageMaxWalletBuy,
+        unitPrice,
+        blockStart,
+        currentStage,
+        currentStageAvailableAmount,
+        currentStageBlockStart,
+        currentStageMaxAmount,
+        paused,
+      }}
+    >
+      {children}
+    </ContractContext.Provider>
+  );
 };
